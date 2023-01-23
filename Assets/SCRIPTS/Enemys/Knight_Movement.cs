5991ecd4f36fd2c8a22fb672c7bb408683084d03
Sydewa <95858551+Sydewa@users.noreply.github.com>
2023-01-23 19:24:57 +0100

    23_01_2023

--- Assets/SCRIPTS/Enemys/Knight_Movement.cs
+++ Assets/SCRIPTS/Enemys/Knight_Movement.cs
@@ -8,31 +8,42 @@ public class Knight_Movement : MonoBehaviour
 {
     KnightState _knightState;
     NavMeshAgent agent;
+    Animator anim;
     Vector3 destination;
 
     
     //Patrol variables ----------------
     Vector3 spawnPosition;
-    float spawnPositionRadius;
     [SerializeField]float patrolRadius;
     float elapsedTime;
+    [SerializeField]Vector2 timeInterval;
+
+    //Animacion variables-----------------------
+    [SerializeField]float smoothTimeWalk;
+
 
     void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
+        anim = GetComponentInChildren<Animator>();
         
     }
     
     void Start()
     {
         spawnPosition = transform.position;
-        destination = new Vector3(Random.Range(-4f, 4f) - spawnPosition.x, transform.position.y, Random.Range(-4f, 4f) - spawnPosition.z);
+
+        //Creamos un vector3 random para empezar el patrol
+        Vector3 randomDirection = Random.insideUnitSphere * patrolRadius;
+        destination = spawnPosition + randomDirection;
     }
 
     // Update is called once per frame
     void Update()
     {
         StateSwitch();
+        AnimationChanges();
+        //Rotation();
     }
 
     void StateSwitch()
@@ -63,29 +74,53 @@ public class Knight_Movement : MonoBehaviour
 
     void Patrol()
     {
-        agent.destination = destination;
-        if(Vector3.Distance(transform.position, destination) < 1f)
+        agent.speed = 1.5f;
+        
+        if(agent.remainingDistance < 0.5f)
         {
             elapsedTime += Time.deltaTime;
 
-            if(Random.Range(1f, 5f) < elapsedTime)
+            if(Random.Range(timeInterval.x, timeInterval.y) < elapsedTime)
             {
                 
-                Vector3 desiredDestination = new Vector3(Random.Range(-4f, 4f) - spawnPosition.x, transform.position.y, Random.Range(-4f, 4f) - spawnPosition.z);
-                //desiredDestination.x = Mathf.Clamp()
-
+                //Vector3 desiredDestination = new Vector3(Random.Range(-4f, 4f) - spawnPosition.x, transform.position.y, Random.Range(-4f, 4f) - spawnPosition.z);
+                Vector3 randomDirection = Random.insideUnitSphere * patrolRadius;
+                destination = spawnPosition + randomDirection;
+                //Debug.Log("Change");
+                elapsedTime = 0;
             }
         }
-        else
-        {
-            elapsedTime = 0;
-        }
+        agent.SetDestination(destination);
+        
     }
 
     void Chase()
     {
         agent.destination = PlayerManager.activeCharacter.transform.position;
+        agent.speed = 3f;
         
 
+    }
+
+    void AnimationChanges()
+    {
+        
+        if(agent.velocity.magnitude > 0)
+        {
+            anim.SetBool("isRunning", true);
+            anim.speed = Mathf.Lerp(anim.speed, agent.velocity.magnitude/2f, smoothTimeWalk * Time.deltaTime);
+            
+        }
+        else
+        {
+            anim.SetBool("isRunning", false);
+            anim.speed = 1f;
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(spawnPosition, patrolRadius);
     }
 }
